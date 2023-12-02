d27420f4a20831ece434237d3977a4c285c50436
Joakim Gyllenskepp <joakim.gyllenskepp@vegvesen.no>
2023-12-02 17:02:55 +0100

    Remove compose from dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("jvm")
-    id("org.jetbrains.compose")
 }
 
 group = "com.aoc"
@@ -12,24 +10,4 @@ repositories {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
-}
-
-dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
-    implementation(compose.desktop.currentOs)
-}
-
-compose.desktop {
-    application {
-        mainClass = "MainKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "aoc2023"
-            packageVersion = "1.0.0"
-        }
-    }
 }
