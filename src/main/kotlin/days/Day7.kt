d83558ed5b398290d6abc966523b8094163a7c31
unknown <jockegy@gmail.com>
2023-12-07 23:22:02 +0100

    Finish day 7 (code not cleaned up)

--- src/main/kotlin/days/Day7.kt
+++ src/main/kotlin/days/Day7.kt
@@ -40,7 +40,42 @@ class Day7 : Day {
             .toString()
     }
 
-    override fun part2(): String = ""
+    // Last answer: 251106438
+    override fun part2(): String {
+        return readRows("Day7")
+            .map { row ->
+                row
+                    .split(" ")
+                    .let {
+                        Hand(
+                            type = it.first().toTypeWithJoker(),
+                            cards = it.first(),
+                            bid = it.last().toInt(),
+                        )
+                    }
+            }
+            .sortedWith { h1, h2 -> Type.values().indexOf(h1.type) - Type.values().indexOf(h2.type) }
+            .groupBy { it.type }
+            .flatMap { (_, hands) ->
+                hands
+                    .sortedWith { h1, h2 ->
+                        h1.cards.toCharArray().zip(h2.cards.toCharArray()).firstNotNullOf { (c1, c2) ->
+                            val c1Value = cardRatingWithJoker.indexOf(c1)
+                            val c2Value = cardRatingWithJoker.indexOf(c2)
+                            if (c1Value == c2Value) {
+                                null
+                            } else {
+                                c1Value - c2Value
+                            }
+                        }
+                    }
+            }
+            .reversed()
+            .also { it.forEach { h -> println(h) } }
+            .mapIndexed { index, hand -> (index + 1) * hand.bid }
+            .sum()
+            .toString()
+    }
 }
 
 val cardRating = listOf(
@@ -75,26 +110,86 @@ enum class Type {
     HighCard,
 }
 
-fun String.toType(): Type {
-    val cardNoOfOccurences = this
+fun String.toType(): Type =
+    this
         .map { c -> Pair(c, this.count { c == it }) }
         .distinctBy { it.first }
         .map { it.second }
-    if (cardNoOfOccurences.contains(5)) {
+        .toType()
+
+fun List<Int>.toType(): Type {
+    if (this.contains(5)) {
         return Type.FiveOfAKind
-    } else if (cardNoOfOccurences.contains(4)) {
+    } else if (this.contains(4)) {
         return Type.FourOfAKind
-    } else if (cardNoOfOccurences.contains(3) && cardNoOfOccurences.contains(2)) {
+    } else if (this.contains(3) && this.contains(2)) {
         return Type.FullHouse
-    } else if (cardNoOfOccurences.contains(3)) {
+    } else if (this.contains(3)) {
         return Type.ThreeOfAKind
-    } else if (cardNoOfOccurences.count { it == 2 } == 2) {
+    } else if (this.count { it == 2 } == 2) {
         return Type.TwoPair
-    } else if (cardNoOfOccurences.contains(2)) {
+    } else if (this.contains(2)) {
         return Type.OnePair
-    } else if (cardNoOfOccurences.size == 5) {
+    } else if (this.size == 5) {
         return Type.HighCard
     } else {
         throw Error("Could not find type  for hand_ $this")
     }
 }
+
+val cardRatingWithJoker = listOf(
+    'A',
+    'K',
+    'Q',
+    'T',
+    '9',
+    '8',
+    '7',
+    '6',
+    '5',
+    '4',
+    '3',
+    '2',
+    'J',
+)
+
+fun String.toTypeWithJoker(): Type =
+    this
+        .also { print("$this: ") }
+        .map { c -> Pair(c, this.count { c == it }) }
+        .let { cards ->
+            val numberOfJs = cards.find { it.first == 'J' }?.second
+            when (numberOfJs) {
+                5 -> {
+                    cards
+                }
+                null -> {
+                    cards
+                }
+                else -> {
+                    val maxOccurences = cards.filter { it.first != 'J' }.maxOf { it.second }
+                    var didUseJs = false
+                    cards
+                        .sortedWith { h1, h2 ->
+                            val c1Value = cardRatingWithJoker.indexOf(h1.first)
+                            val c2Value = cardRatingWithJoker.indexOf(h2.first)
+                            c1Value - c2Value
+                        }
+                        .mapNotNull {
+                            if (it.second == maxOccurences && it.first != 'J' && !didUseJs) {
+                                didUseJs = true
+                                Pair(it.first, it.second + numberOfJs)
+                            } else if (it.first == 'J') {
+                                null
+                            } else {
+                                it
+                            }
+                        }
+                }
+            }
+        }
+        .distinctBy { it.first }
+        .also { print(it) }
+        .map { it.second }
+        .toType()
+        .also { println(" -> $it") }
