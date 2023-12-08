016fd92ae71ad6c3a8abd4b8eadf3340122b78db
Joakim Gyllenskepp <joakim.gyllenskepp@vegvesen.no>
2023-12-08 22:09:34 +0100

    Day 8 part 2

--- src/main/kotlin/days/Day8.kt
+++ src/main/kotlin/days/Day8.kt
@@ -20,15 +20,44 @@ class Day8 : Day {
                         }
                     }
             }
-            return "Did not find"
+            return "Did not find ZZZ"
         }
 
-    override fun part2(): String = "Part2"
+    /** Least common multiple? */
+    override fun part2(): String = readRows("Day8_2")
+        .let { rows ->
+            val networkMap = rows.toNetworkMap()
+            val instructions = rows[0]
+            var currentNodes = networkMap.keys.filter { it.last() == 'A' }
+            val denominators: MutableMap<Int, Long?> = List(currentNodes.size) { index -> index to null }.toMap().toMutableMap()
+
+            for (i in 0..Int.MAX_VALUE) {
+                if (denominators.values.all { it != null }) {
+                    break
+                }
+                currentNodes = currentNodes.mapIndexed { index, currentNode ->
+                    instructions[i % instructions.length]
+                        .readInstruction(currentNode, networkMap)
+                        .let { newNode ->
+                            if (newNode.last() == 'Z') {
+                                denominators[index] = i + 1L
+                            }
+                            newNode
+                        }
+                }.also { newNodes ->
+                    if (newNodes.all { it.last() == 'Z' }) {
+                        return (i + 1).toString()
+                    }
+                }
+            }
+
+            return denominators.values.toList().filterNotNull().reduce(::lcm).toString()
+        }
 }
 
 private fun List<String>.toNetworkMap() = this.drop(2)
     .associate { row ->
-        Regex("[A-Z]+").findAll(row)
+        Regex("[1-9A-Z]+").findAll(row)
             .map { match -> match.value }.toList()
             .let { it[0] to Pair(it[1], it[2]) }
     }
@@ -37,4 +66,12 @@ private fun Char.readInstruction(key: String, networkMap: Map<String, Pair<Strin
     'L' -> networkMap[key]?.first ?: throw Error("Did not find key $key")
     'R' -> networkMap[key]?.second ?: throw Error("Did not find key $key")
     else -> throw Error("Invalid instruction $this")
+}
+
+private fun lcm(first: Long, second: Long): Long {
+    return first * second / gcd(first, second)
+}
+
+private fun gcd(first: Long, second: Long): Long {
+    return if (second == 0L) first else gcd(second, first % second)
 }
